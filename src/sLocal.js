25626c7898a2aab784b2ab8263d365ecbd9d0d33
Gen Hames <hamesg@tutanota.de>
2020-12-14 14:32:52 -0800

    Reactions now return a function that is the meta object built-in

--- src/sLocal.js
+++ src/sLocal.js
@@ -1,122 +1,87 @@
 let boxId = 0;
 let reactionId = 0;
 
-// Registry of functions to reaction metadata objects
-let live = new Map();
-// The reaction meta info to bind subs in runReaction
+// Registry of reactions
+let live = new Set();
+// Current reaction
 let rxActive = undefined;
 // To skip the subbed consistency check during an s(box) read
 let sRead = false;
 
 function rx(fn) {
-  if (live.has(fn)) {
-    throw new Error('Already a live reaction');
-  }
-  const meta = {
-    id: `R${reactionId++}:${fn.name || '<Anon>'}`,
-    rx: fn,
-    subbed: new Set(),     // Set<Box>
-    passed: new Set(),     // Set<Box>
-    runs: 0,
-    rxParent: rxActive,    // ReactionMeta || undefined
-    rxChildren: new Set(), // Set<ReactionMeta>
-  };
-  // TODO: How is this registry working?
-  live.set(fn, meta);
-  console.group(`Create ${meta.id}`);
+  const _rx = () => _rxRun(_rx);
+  _rx.id = `R${reactionId++}:${fn.name || '<Anon>'}`;
+  _rx.fn = fn;
+  _rx.sr = new Set(); // Set<Box>
+  _rx.pr = new Set(); // Set<Box>
+  _rx.runs = 0;
+  _rx.created = new Set(); // Set<ReactionMeta>
+  _rx.unsubscribe = () => _rxUnsubscribe(_rx);
+  console.log(`Create ${_rx.id}`);
+  live.add(_rx);
   if (rxActive) {
-    console.log(`Reaction lifetime scoped to its parent, ${rxActive.id}`);
-    rxActive.rxChildren.add(meta);
+    console.log(`${_rx.id} was created under ${rxActive.id}`);
+    rxActive.created.add(_rx);
   }
-  let error;
-  try {
-    _rxRun(meta);
-  } catch (err) {
-    console.log(`Creation error: ${err.message}}`);
-    _rxUnsubscribe(meta);
-    error = err;
-  }
-  console.groupEnd(`Create ${meta.id}`);
-  if (error) throw error;
-
-  const _rx = () => _rxRun(meta);
-  _rx.unsubscribe = () => _rxUnsubscribe(meta);
-  _rx.meta = meta;
+  _rx();
   return _rx;
 }
 
 // This takes a meta object because honestly you shouldn't use it directly?
-function _rxRun(meta) {
-  console.group(`Run ${meta.id}`);
-  const prevActive = rxActive;
-  rxActive = meta;
-  // For cleanup later. Save work via pointer move instead of duplicating set
-  const prevSubs = meta.subbed;
-  meta.subbed = new Set();
-  meta.passed.clear();
-
+function _rxRun(rx) {
   // Define the subscription function
   const s = box => {
-    if (!box.id || !box.reactions) {
-      throw new Error('s() Parameter isn\'t a box');
-    }
-    console.log(`s() ${meta.id} 🔗 ${box.id}`);
-    if (meta.passed.has(box)) {
-      throw new Error(`Reaction ${meta.id} can't subscribe-read to ${box.id} after pass-reading it; pick one`);
-    }
-    // Add after checking to be idempotent
-    meta.subbed.add(box);
-    box.reactions.add(meta);
+    if (rx.pr.has(box)) throw new Error(`Mixed reads ${box.id}`);
+    // Add to box.rx first so it throws if s() wasn't passed a box...
+    box.rx.add(rx);
+    rx.sr.add(box);
+    console.log(`s() ${rx.id} 🔗 ${box.id}`);
     sRead = true;
     const value = box();
     sRead = false;
     return value;
   };
 
+  console.group(`Run ${rx.id}`);
+  const prevActive = rxActive;
+  rxActive = rx;
+  // Drop everything in the tree like Sinuous'/S.js' automatic memory management
+  _rxUnsubscribe(rx);
   let error;
   try {
-    meta.rx(s);
-    meta.runs++;
-    console.log(`Run ${meta.runs}: ${meta.subbed.size}/${meta.subbed.size + meta.passed.size} reads subscribed`);
-    // If a reaction doesn't sub a previously subbed box then that box doesn't
-    // need to run the reaction anymore
-    meta.subbed.forEach(box => prevSubs.delete(box));
-    prevSubs.forEach(box => {
-      console.log(`Unsubscribing from unused box ${box.id}`);
-      box.reactions.delete(meta);
-    });
-    // This operation is independent of the above work with prevSubs
-    if (meta.subbed.size === 0) {
-      _rxUnsubscribe(meta);
-    }
+    rx.fn(s);
+    rx.runs++;
+    if (rx.runs > 10) throw new Error();
+    console.log(`Run ${rx.runs}: ${rx.sr.size}/${rx.sr.size + rx.pr.size} reads subscribed`);
   } catch (err) {
     error = err;
   }
   rxActive = prevActive;
-  console.groupEnd(`Run ${meta.id}`);
+  console.groupEnd(`Run ${rx.id}`);
   if (error) throw error;
 }
 
-function _rxUnsubscribe(meta) {
-  console.log(`Unsubscribing reaction ${meta.id}`);
-  if (meta.rxParent) {
-    meta.rxParent.rxChildren.delete(meta);
+function _rxUnsubscribe(rx) {
+  if (rx.created.size) {
+    rx.created.forEach(_rxUnsubscribe);
+    rx.created = new Set();
   }
-  meta.rxChildren.forEach(_rxUnsubscribe);
-  meta.subbed.forEach(box => box.reactions.delete(meta));
+  rx.sr.forEach(box => box.rx.delete(rx));
+  rx.sr = new Set();
+  rx.pr = new Set();
 }
 
 function _box(k, v) {
   // Store in a closure and not a property of the box
-  // Reads should always be safe so there's no reason to backdoor it
   let saved = v;
   const box = (...args) => {
     if (args.length) {
       const [valueNext] = args;
-      console.log(`Write ${box.id}:`, saved, '➡', valueNext, `Notifying ${box.reactions.size} reactions`);
+      console.log(`Write ${box.id}:`, saved, '➡', valueNext, `Notifying ${box.rx.size} reactions`);
       saved = valueNext;
-      box.reactions.forEach(_rxRun);
-      // Don't return a value. Keeps it simple if write doesn't also read
+      // Duplicate the set else it's an infinite loop...
+      new Set(box.rx).forEach(_rxRun);
+      // Don't return a value; keeps it simple
       return;
     }
     if (rxActive) {
@@ -126,28 +91,21 @@ function _box(k, v) {
       );
     }
     if (rxActive && !sRead) {
-      if (rxActive.subbed.has(box)) {
-        throw new Error(`Reaction ${rxActive.id} can't pass-read ${box.id} after subscribe-reading it; pick one`);
-      }
-      // Add after checking to be idempotent
-      rxActive.passed.add(box);
+      if (rxActive.sr.has(box)) throw new Error(`Mixed reads ${box.id}`);
+      rxActive.pr.add(box);
     }
     return saved;
   };
   box.id = `B${boxId++}` + (k ? `:${k}` : '');
-  box.reactions = new Set();
+  box.rx = new Set();
   return box;
 }
 
-function boxes(bundle) {
-  for (const [k, v] of Object.entries(bundle)) {
-    bundle[k] = _box(k, v);
+function boxes(obj) {
+  for (const k of Object.keys(obj)) {
+    obj[k] = _box(k, obj[k]);
   }
-  return bundle;
+  return obj;
 }
 
-module.exports = {
-  live,
-  rx,
-  boxes,
-};
+module.exports = { live, rx, boxes };
--- test.js
+++ test.js
@@ -7,8 +7,10 @@ const data = boxes({ label: 'Ok', count: 0 });
 const r0 = rx(() => {
   console.log('Hey!', data.count());
 });
-console.log('r0', r0.meta.subbed);
+console.log('r0', r0.sr);
 console.log('live', live);
+
+r0();
 
 console.log('r0.unsubscribe');
 r0.unsubscribe();
@@ -20,8 +22,22 @@ data.count(10);
 const r1 = rx(s => {
   console.log('Hey!', s(data.count));
 });
-console.log('r1', r1.meta.subbed);
+console.log('r1', r1.sr);
 
 console.log('live', live);
 
 data.count(100);
+
+// Pause
+r1.unsubscribe();
+
+console.log('data.count next');
+data.count(101);
+console.log('data.count next');
+data.count(110);
+
+// Start
+r1();
+
+console.log('data.count next');
+data.count(111);
